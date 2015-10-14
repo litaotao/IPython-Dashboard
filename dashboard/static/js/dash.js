0f55af19df6e83395ccd0f694654edfb352c3a94
charles.li <charles.upboy@gmail.com>
2015-10-14 23:54:37 +0800

    replace gridster with gridstack almost done, will put more work on the server later on

--- dashboard/static/js/dash.js
+++ dashboard/static/js/dash.js
@@ -1,58 +1,58 @@
-
-function onmouseoverChange(obj)
-    {
-        obj.style.borderColor="#FF6600";
-        obj.style.borderWidth="1px";
-        obj.style.backgroundColor="FFFFCC";
-    }
-
-function add_box() {
-    gridster = $(".gridster > ul").gridster({}).data('gridster');
-    next_index = gridster.$widgets.length + 1;
-    var box = "<li data-row=\"1\" data-col=\"1\" data-sizex=\"1\" data-sizey=\"1\" class=\"box-li\">  \
-              <div class=\"chart-wrapper\"> \
-                <div class=\"chart-title bold\"> \
-                  <table class=\"table input-title-level-2\"> \
-                    <tr class=\"active\" style=\"padding-left: 10%;\"> \
-                      <td style=\"padding: 0px; width: 90%; padding-left: inherit\"><input class=\"form-control input-lg input-title-level-2\" maxlength=\"32\" placeholder=\"Naming your graph\"></td> \
-                      <td style=\"padding: 0px; width: 10%\"><button class=\"btn btn-primary edit-button\" data-toggle=\"modal\" data-target=\"#myModal\"><i class=\"fa fa-fw fa-lg fa-edit\" style=\"color: black;\"></i></button></td> \
-                    </tr> \
-                  </table>  \
-                </div> \
-                <div class=\"chart-graph\"> \
-                  <iframe src=\" \" style=\"width: 100%; height: 100%; border: initial;\"></iframe> \
-                </div> \
-              </div> \
-            </li>"
-    gridster.add_widget(box);
-}
-
-
-function init_grid(){   //DOM Ready
-    var gridster;
-    var box_width = Math.floor(document.getElementById("box-ul").offsetWidth / 2)
-    var box_height = Math.floor(box_width * 9 / 16)
-    
-    gridster = $(".gridster ul").gridster({
-      widget_base_dimensions: [box_width - 5, box_height - 5],
-      widget_margins: [10, 10],
-    }).data('gridster');
-}
-
-
+// re-arrange when mouse hover on the side bar
 function resize_content(direction) {
-    var padding = (direction==1) ? "100px" : "0px";
-    document.getElementById("main-content").style.paddingLeft = padding;
+  var padding = (direction==1) ? "100px" : "0px";
+  document.getElementById("main-content").style.paddingLeft = padding;
 }
 
 
+// initialize gridstack configure
+function initGridstack(){
+  var options = {
+    width: 12,
+    animate: true, 
+    vertical_margin: 5,
+    resizable: {handles: "e, se, s, sw, w"}
+  };
+  $('.grid-stack').gridstack(options);
+}
 
-/*
+// initialze 4 grids
+function createGrids(){
+  grid = $('.grid-stack').data('gridstack');
+  box = '<div data-gs-min-height="4" data-gs-min-width="6">   \
+          <div class="grid-stack-item-content">              \
+            <div class="chart-wrapper">                      \
+              <div class="chart-title bold">                 \
+                <table class="table input-title-level-2">    \
+                  <tr class="active" style="padding-left: 10%;">   \
+                    <td style="padding: 0px; width: 90%; padding-left: inherit">   \
+                      <input class="form-control input-lg input-title-level-2" maxlength="32" placeholder="Naming your graph">  \
+                    </td>                                                                                                       \
+                    <td style="padding: 0px; width: 10%">                                                                       \
+                      <button class="btn btn-primary edit-button" data-toggle="modal" data-target="#myModal">                   \
+                        <i class="fa fa-fw fa-lg fa-edit" style="color: black;" onclick="addClassMark(this)"></i></button>      \
+                    </td>   \
+                  </tr>     \
+                </table>    \
+              </div>        \
+              <div id="test_graph" class="chart-graph" style="width: 100%; overflow: auto; color: #444;">   \
+              </div>        \
+            </div>          \
+          </div>   \
+        </div>';
+  // initialized four boxes
+  grid.add_widget(box, 0, 0, 6, 5)
+  grid.add_widget(box, 6, 0, 6, 5);
+  grid.add_widget(box, 0, 1, 6, 5)
+  grid.add_widget(box, 6, 1, 6, 5);
+}
+
+/************************************
 Interact with server
-*/
-
+*************************************/
+// get all the keys from server
 function getKeys(){
-    $("[data-target]").on("click", function(){
+  $("[data-target]").on("click", function(){
     var keys_select = $("#keys");
     keys_select.empty();
 
@@ -66,118 +66,117 @@ function getKeys(){
   })
 }
 
-
+// get the value for a key and parse it as a table by default
 function getValue(){
-    // var selectDOM = $("#keys")[0];
-    // var key = selectDOM.options[selectDOM.selectedIndex].text;
-    $("#keys").on("change", function(){
-        var selectDOM = $("#keys")[0];
-        var key = selectDOM.options[selectDOM.selectedIndex].text;
+  $("#keys").on("change", function(){
+    var selectDOM = $("#keys")[0];
+    var key = selectDOM.options[selectDOM.selectedIndex].text;
 
-        var url = "http://127.0.0.1:9090/key/" + key;
-        // console.log(url);
+    var url = "http://127.0.0.1:9090/key/" + key;
+      // console.log(url);
 
-        $.getJSON(url, function(data){
-            // console.log(data);
-            parseTable($.parseJSON(data.data));
-        })
-    });
+    $.getJSON(url, function(data){
+        // console.log(data);
+        parseTable($.parseJSON(data.data));
+    })
+  });
 }
 
 
 function parseTable(data){
-    function genElement(type){
-        var element = document.createElement(type);
-        if (type == "td"){
-            element.setAttribute("nowrap", "nowrap");
-        }
-        return element;
+  function genElement(type){
+    var element = document.createElement(type);
+    if (type == "td"){
+      element.setAttribute("nowrap", "nowrap");
     }
-    var table = genElement("table");
-    var thead = genElement("thead");
-    var tbody = genElement("tbody");
+    return element;
+  }
+  var table = genElement("table");
+  var thead = genElement("thead");
+  var tbody = genElement("tbody");
+  var tr = genElement("tr");
+  var th = genElement("th");
+  var td = genElement("td");
+
+  tr.appendChild(th);
+  
+  var columns = [];
+  $.each(data, function(key, value){
+    var th = genElement("th");
+    th.innerText = key;
+    tr.appendChild(th);
+    columns.push(key);        
+  })
+  thead.appendChild(tr);
+
+  var indexes = [];
+
+  $.each(data[columns[0]], function(index, value){
+    indexes.push(index);
+  })
+
+  for (var row = 0; row < 20; row++) {
     var tr = genElement("tr");
     var th = genElement("th");
-    var td = genElement("td");
-
+    th.innerText = indexes[row];
     tr.appendChild(th);
-    
-    var columns = [];
-    $.each(data, function(key, value){
-        var th = genElement("th");
-        th.innerText = key;
-        tr.appendChild(th);
-        columns.push(key);        
+    $.each(columns, function(no_user, col){
+      var td = genElement("td");
+      td.innerText = data[col][indexes[row]];
+      tr.appendChild(td);
     })
-    thead.appendChild(tr);
+    tbody.appendChild(tr);
+  };
+  
+  table.setAttribute("id", "table_value");
+  table.setAttribute("border", "1px");
+  table.className = "table-condensed table-hover";
+  table.style.fontSize = "small";
+  table.style.fontWeight = "400";
 
-    var indexes = [];
+  // remove table
+  var tableDOM = $("#value")[0]
+  currentTable = tableDOM.children
+  for (var i = currentTable.length - 1; i >= 0; i--) {
+    tableDOM.removeChild(currentTable[0]);
+  };
 
-    $.each(data[columns[0]], function(index, value){
-        indexes.push(index);
-    })
-
-    for (var row = 0; row < 20; row++) {
-        var tr = genElement("tr");
-        var th = genElement("th");
-        th.innerText = indexes[row];
-        tr.appendChild(th);
-        $.each(columns, function(no_user, col){
-            var td = genElement("td");
-            td.innerText = data[col][indexes[row]];
-            tr.appendChild(td);
-        })
-        tbody.appendChild(tr);
-    };
-    
-    table.setAttribute("id", "table_value");
-    table.setAttribute("border", "1px");
-    table.className = "table-condensed table-hover";
-    table.style.fontSize = "small";
-    table.style.fontWeight = "400";
-
-    // remove table
-    var tableDOM = $("#value")[0]
-    currentTable = tableDOM.children
-    for (var i = currentTable.length - 1; i >= 0; i--) {
-        tableDOM.removeChild(currentTable[0]);
-    };
-
-    // add table
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    tableDOM.appendChild(table);
-    // console.log(tableDOM.innerHTML);
+  // add table
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  tableDOM.appendChild(table);
+  // console.log(tableDOM.innerHTML);
 }
 
 
 
 $(document).ready(function() {
-    getKeys();
-    getValue();
+  initGridstack();
+  createGrids();
+  getKeys();
+  getValue();
 });
 
 
 function addClassMark(obj){
-    obj.classList.add("edit-graph");
+  obj.classList.add("edit-graph");
 }
 
 
 function saveGraph(){
-    // var data = $("#value")[0].cloneNode(true);
-    var data = $("#value")[0].children[0].cloneNode(true);
-    data.removeAttribute("id");
-    data.style.height = "100%";
-    data.style.width = "100%";
-    var obj = $("td > button > i.edit-graph")[0];
-    var res = obj.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.children[1]
-    res.innerHTML = "";
-    res.appendChild(data);
-    a1 = data;
-    a2 = res;
+  var data = $("#value")[0].children[0].cloneNode(true);
+  data.removeAttribute("id");
+  data.style.height = "100%";
+  data.style.width = "100%";
+  var obj = $("td > button > i.edit-graph")[0];
+  var res = obj.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.children[1]
+  res.innerHTML = "";
+  res.appendChild(data);
+  a1 = data;
+  a2 = res;
 }
 
 
 function test(){
-    console.log("test");
+  console.log("test");
 }
