8589157e4f614bacdad95ac8e1e5e7078ca864dd
litaotao <charles.upboy@gmail.com>
2015-10-16 11:57:15 +0800

    move on server on-going, everything goes as planned, thanks flask, making web dev so easy

---
+++ dashboard/server/resources/home.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# built-in package
+import json
+
+# third-party package
+from flask import render_template, make_response
+from flask.ext.restful import Resource
+
+# user-defined package
+from dashboard import r_db, config
+from ..utils import build_response
+
+
+class Home(Resource):
+    """home page
+    """
+    def get(self):
+        # headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('dashboard.html'))
+
+
+
--- dashboard/server/views.py
+++ dashboard/server/views.py
@@ -3,12 +3,17 @@
 from dashboard import api
 from resources.storage import Key, KeyList
 from resources.dash import Dash, DashList
+from resources.home import Home
+from resources.test import Test
+
 
 
 '''
 # Restful API
 '''
+api.add_resource(Home, '/')
 api.add_resource(KeyList, '/keys/', '/keylist/')
 api.add_resource(Key, '/keys/<string:key>')
 api.add_resource(DashList, '/dashes/', '/dashlist/')
 api.add_resource(Dash, '/dashes/<string:dash_id>')
+api.add_resource(Test, '/home')
