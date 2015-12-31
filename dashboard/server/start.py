b2ab0054141f26f0de566176b688fb52ce5a2b88
taotao <charles.upboy@gmail.com>
2015-12-31 15:57:33 +0800

    update startup script and travis config

--- dashboard/server/start.py
+++ dashboard/server/start.py
@@ -13,8 +13,8 @@ from .. import app
 
 parser = argparse.ArgumentParser(description='Start your IPython-Dashboard server ...')
 parser.add_argument('-H', '--host', type=str, default='0.0.0.0', help='server host, default localhost')
-parser.add_argument('-p', '--port', type=int, default=9090, help='server port, default 9090')
-parser.add_argument('-d', '--debug', type=bool, default=True, help='server port, default true')
+parser.add_argument('-P', '--port', type=int, default=9090, help='server port, default 9090')
+parser.add_argument('-D', '--debug', type=bool, default=True, help='server port, default true')
 
 args = parser.parse_args()
 print(args)
